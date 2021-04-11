228ad8732f479fc2acd520347ec41d96e9bb8eda
HerbertSousa <herberti1405@outlook.com>
2021-04-11 11:56:22 -0300

    github explorer

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import '../styles/repositories.scss'
+import { RepositoryItem } from "./RepositoryItem";
+
+export function RepositoryList() {
+    const [ repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, [repositories])
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositorios</h1>
+
+            <ul>
+                {repositories.map(repository => <RepositoryItem key={repository.name} repository={repository} />)}
+            </ul>
+        </section>
+    );
+}
